b481f0abd50c581677534b0bb4f27d01f7cd8b0e
Maddy Ali <maddyoali@gmail.com>
2023-04-26 17:36:41 -0500

    Render search result client side

--- public/js/main.js
+++ public/js/main.js
@@ -18,8 +18,45 @@ async function hitAPI() {
       `https://the-coding-resources-api.up.railway.app/api/${keyword}`
     );
     const data = await response.json();
-    console.log(data);
+    render(data);
   } catch (error) {
     console.log(error);
+  }
+  input.value = "";
+}
+
+function render(data) {
+  const resultList = document.getElementById("result-list");
+  resultList.innerHTML = "";
+
+  if (data.length) {
+    data.forEach((el) => {
+      const li = document.createElement("li");
+
+      li.innerHTML = `
+        <pre class="json">
+        <code>
+            {
+                <div class="indent">
+                    <p>name: '${el.name}',</p>
+                    <p class="text-truncate">url: '<a href="${
+                      el.url
+                    }" target="_blank">${el.url}</a>',</p>
+                    <p>description: '${el.description}',</p>
+                    <p class="text-truncate">keywords: [${el.keywords
+                      .map((keyword) => `"${keyword}"`)
+                      .join(", ")}]</p>
+                </div>
+            },
+        </code>
+      </pre>
+        `;
+
+      resultList.appendChild(li);
+    });
+  } else {
+    const li = document.createElement("li");
+    li.innerHTML = "No matches found.";
+    resultList.appendChild(li);
   }
 }
